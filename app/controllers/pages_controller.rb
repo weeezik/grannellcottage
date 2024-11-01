21894a722c0682dd4f834fc798acedc59851ec6b
weeezik <mkihanya.33@gmail.com>
2024-11-01 11:41:32 -0700

    Begin routing with aboutus test

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,11 @@
 class PagesController < ApplicationController
   def index
-    
+  end
+  def aboutus
+  end
+  def history
+  end
+
+  def catalog
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "pages#index"
 
-  
+  get "/aboutus", to: "pages#aboutus"
+
 end
